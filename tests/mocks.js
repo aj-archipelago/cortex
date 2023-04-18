0f9388705079fe76480384f061afc2175f4246ec
Jason McCartney <jmac@theroot.org>
2023-04-18 16:38:33 -0700

    Added pathwayResolver test

--- tests/mocks.js
+++ tests/mocks.js
@@ -7,6 +7,7 @@ export const mockConfig = {
         models: {
           testModel: {
             url: 'https://api.example.com/testModel',
+            type: 'OPENAI-COMPLETION',
           },
         },
       };
