ba73147d8bef46c3af4770fbeff4e3a828820dee
ademgcl <ademgcl@gmail.com>
2024-01-16 13:04:02 +0300

    corrected truncation of text elements

--- server/plugins/openAiVisionPlugin.js
+++ server/plugins/openAiVisionPlugin.js
@@ -1,12 +1,11 @@
 import OpenAIChatPlugin from './openAiChatPlugin.js';
 
+
 class OpenAIVisionPlugin extends OpenAIChatPlugin {
 
-    getRequestParameters(text, parameters, prompt) {
-        const requestParameters = super.getRequestParameters(text, parameters, prompt);
-
-        //check if elements of requestMessages strings are JSON, if valid JSON parse them to obj
-        requestParameters.messages.map(message => {
+    tryParseMessages(messages) {
+        //check if elements of messages strings are JSON, if valid JSON parse them to obj
+        messages.map(message => {
             try {
                 // message.content can be array or string
                 if (typeof message.content === 'string') message.content = JSON.parse(message.content);
@@ -16,6 +15,55 @@ class OpenAIVisionPlugin extends OpenAIChatPlugin {
                 return message;
             }
         });
+        return messages;
+    }
+
+    truncateMessagesToTargetLength(messages, targetLength) {
+
+        this.tryParseMessages(messages);
+        
+        const messagesList = [];
+        for(const message of messages){
+            const {content} = message;
+            if(typeof content === 'string') messagesList.push(content);
+            else if(Array.isArray(content)) {
+                for(const item of content){
+                    if(typeof item === 'string') messagesList.push(item);
+                    else messagesList.push(item?.text || "");
+                }
+            }
+        }
+        const truncated = super.truncateMessagesToTargetLength(messagesList, targetLength);
+
+        //set truncated back if any
+        let i = 0;
+        for(const message of messages){
+            if(typeof content === 'string') message.content = truncated[i++];
+            else if(Array.isArray(message.content)) {
+                for(let i=0;i<message.content.length;i++){
+                    if(typeof message.content[i] === 'string'){
+                        if(message.content[i] != truncated[i]) {
+                            console.log(`message truncation = original: ${message.content[i]}, truncated: ${truncated[i].content}`);
+                            message.content[i] = truncated[i];
+                        }
+                    } 
+                    else if(message.content[i].text) {
+                        if(message.content[i].text != truncated[i]) {
+                            console.log(`message truncation = original: ${message.content[i].text}, truncated: ${truncated[i].content}`);
+                            message.content[i].text = truncated[i].content;
+                        }
+                    }
+                }
+            }
+        }
+
+        return messages;
+    }
+
+    getRequestParameters(text, parameters, prompt) {
+        const requestParameters = super.getRequestParameters(text, parameters, prompt);
+
+        this.tryParseMessages(requestParameters.messages);
 
         if(this.promptParameters.max_tokens) {
             requestParameters.max_tokens = this.promptParameters.max_tokens;
