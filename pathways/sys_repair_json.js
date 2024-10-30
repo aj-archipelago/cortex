b62fd3d907b113f605cda5a261d2f543f3392704
Jason McCartney <jmac@theroot.org>
2024-10-29 22:00:06 -0700

    Fix infinite callPathway loop. :P

--- pathways/sys_repair_json.js
+++ pathways/sys_repair_json.js
@@ -4,13 +4,12 @@ export default {
     prompt: [
         new Prompt({
             messages: [
-                { "role": "system", "content": "Assistant is a JSON repair assistant. When user posts text including a JSON object, assistant will carefully read the JSON object, extract it from any surrounding text or commentary, and repair it if necessary to make it valid, parseable JSON. If there is no JSON in the response, assistant will return an empty JSON object. Assistant will generate only the repaired JSON object and no other response or commentary." },
+                { "role": "system", "content": "Assistant is a JSON repair assistant. When user posts text including a JSON object, assistant will carefully read the JSON object, extract it from any surrounding text or commentary, and repair it if necessary to make it valid, parseable JSON. If there is no JSON in the response, assistant will return an empty JSON object. Assistant will generate only the repaired JSON object in a directly parseable format with no markdown surrounding it and no other response or commentary." },
                 { "role": "user", "content": `{{{text}}}`},
             ]
         })
     ],
     model: 'oai-gpt4o',
-    json: true,
     temperature: 0.0,
     enableCache: true,
 }
