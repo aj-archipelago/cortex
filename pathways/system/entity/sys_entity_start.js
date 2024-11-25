1eec4b8b004de0eabe7ffe189734b0b5b2c3fc78
Jason McCartney <jmac@theroot.org>
2024-11-24 23:34:12 -0700

    More tuning / optimization

--- pathways/system/entity/sys_entity_start.js
+++ pathways/system/entity/sys_entity_start.js
@@ -2,7 +2,7 @@
 // Beginning of the rag workflow for Jarvis
 import { callPathway } from '../../../lib/pathwayTools.js';
 import logger from  '../../../lib/logger.js';
-import { chatArgsHasImageUrl, convertToSingleContentChatHistory } from  '../../../lib/util.js';
+import { chatArgsHasImageUrl } from  '../../../lib/util.js';
 import { QueueServiceClient } from '@azure/storage-queue';
 import entityConstants from './shared/sys_entity_constants.js';
 
@@ -38,6 +38,8 @@ export default {
     useInputChunking: false,
     enableDuplicateRequests: false,
     model: 'oai-gpt4o',
+    anthropicModel: 'claude-35-sonnet-vertex',
+    openAIModel: 'oai-gpt4o',
     inputParameters: {
         privateData: false,    
         chatHistory: [{role: '', content: []}],
@@ -57,22 +59,28 @@ export default {
     tokenRatio: TOKEN_RATIO,
     ...entityConstants,
 
-    resolver: async (_parent, args, contextValue, _info) => {
-        let title = null; // chat_title response, contains possible update to chat title
+    executePathway: async ({args, resolver}) => {
+        let title = null;
         let codeRequestId = null;
-        args = { ...args, ...entityConstants };
-        
-        const { pathwayResolver } = contextValue;
-        
+
+        args = {
+            ...args,
+            ...entityConstants
+        };
+
+        // Limit the chat history to 20 messages to speed up processing
+        args.chatHistory = (args.messages || args.chatHistory).slice(-20);
+        const pathwayResolver = resolver;
+        const { anthropicModel, openAIModel } = pathwayResolver.pathway;
+
+        const styleModel = args.aiStyle === "Anthropic" ? anthropicModel : openAIModel;
+
         // if the model has been overridden, make sure to use it
         if (pathwayResolver.modelName) {
             args.model = pathwayResolver.modelName;
         }
         
         const fetchChatResponse = async (args) => {
-
-            const styleModel = args.aiStyle === "Anthropic" ? "claude-35-sonnet-vertex" : "oai-gpt4o";
-
             const [chatResponse, chatTitleResponse] = await Promise.all([
                 callPathway('sys_generator_quick', {...args, model: styleModel}),
                 callPathway('chat_title', { ...args, text: args.chatHistory.map(message => message.content).join("\n")}),
@@ -84,36 +92,18 @@ export default {
         };
             
         const { chatHistory } = args;
-        const visionContentPresent = chatArgsHasImageUrl(args);
-        //const multiModalChatHistory = JSON.parse(JSON.stringify(chatHistory));
 
-        // Convert chatHistory to text only for rest of the code
-        // after this chatHistory is no longer multi-modal
-        convertToSingleContentChatHistory(chatHistory);
-      
-        // get the last user message to use as a context
-        const contextInfo = chatHistory.filter(message => message.role === "user").slice(0, -1).map(message => message.content).join("\n");
-        
         // start fetching the default response - we may need it later
-        const fetchChatResponsePromise = fetchChatResponse(args);
+        const fetchChatResponsePromise = fetchChatResponse({ ...args });
+
+        const visionContentPresent = chatArgsHasImageUrl(args);
 
         try {
-            // Execute the router options in parallel
-            let promises = {
-                searchRequiredResponse: callPathway('sys_router_search', { ...args, contextInfo }),
-                toolRequiredResponse: callPathway('sys_router_expert', { ...args, contextInfo }),
-            };
-
-            const results = await Promise.all(Object.values(promises));
-
-            // Here results will have the resolved values in the same order as Object.values(promises)
-            // You can then map these results back into an object
-            let response = {};
-            Object.keys(promises).forEach((key, i) => {
-                response[key] = results[i];
+            // Get tool routing response
+            const toolRequiredResponse = await callPathway('sys_router_tool', { 
+                ...args,
+                chatHistory: chatHistory.slice(-4) 
             });
-
-            const { searchRequiredResponse, toolRequiredResponse } = response;
 
             // Asynchronously manage memory for this context
             if (args.aiMemorySelfModify) {
@@ -121,21 +111,16 @@ export default {
                 .catch(error => logger.error(error?.message || "Error in sys_memory_manager pathway"));
             }
 
-            const parsedSResponse = searchRequiredResponse ? JSON.parse(searchRequiredResponse) : {};
-            const parsedEResponse = toolRequiredResponse ? JSON.parse(toolRequiredResponse) : {};
-            const { searchRequired } = parsedSResponse;
-            const { toolRequired, toolMessage, toolFunction } = parsedEResponse;
-            let toolCallbackName = null;
-            let toolCallbackId = null;
-            let toolCallbackMessage = null;
+            const { toolRequired, toolMessage, toolFunction } = JSON.parse(toolRequiredResponse || '{}');
+            let toolCallbackName, toolCallbackId, toolCallbackMessage;
 
-            logger.info(`searchRequired: ${searchRequired}, toolRequired: ${toolRequired}, toolFunction: ${toolFunction}`);
+            logger.info(`toolRequired: ${toolRequired}, toolFunction: ${toolFunction}`);
 
             if (toolRequired && toolFunction) {
                 switch (toolFunction.toLowerCase()) {
                     case "codeexecution":
                         {
-                            const codingRequiredResponse = await callPathway('sys_router_code', { ...args, contextInfo });
+                            const codingRequiredResponse = await callPathway('sys_router_code', { ...args });
                             let parsedCodingRequiredResponse;
                             try {
                                 parsedCodingRequiredResponse = JSON.parse(codingRequiredResponse || "{}");
@@ -161,56 +146,48 @@ export default {
                         }
                         break;
                     case "image":
-                        if (toolRequired) {
-                            toolCallbackName = 'sys_generator_image';
-                            toolCallbackMessage = toolMessage;
-                            //const imageResponse = await callPathway('sys_generator_image', { ...args, useMemory: true }, pathwayResolver);
-                            //pathwayResolver.tool = JSON.stringify({ search: false, title });
-                            //return JSON.stringify({response: imageResponse});
-                        }
+                        toolCallbackName = 'sys_generator_image';
+                        toolCallbackMessage = toolMessage;
                         break;
                     case "vision":
+                        break;
                     case "video":
                     case "audio":
                     case "pdf":
                         if (visionContentPresent) {
                             toolCallbackName = 'sys_generator_video_vision';
                             toolCallbackMessage = toolMessage;
-                            //const videoResponse = await callPathway('sys_generator_video_vision', { ...args, chatHistory: multiModalChatHistory }, pathwayResolver);
-                            //pathwayResolver.tool = JSON.stringify({ search: false, title });
-                            //return JSON.stringify({response: videoResponse});
                         }
                         break;
                     case "code":
                     case "write":
-                        if (toolRequired) {
-                            toolCallbackName = 'sys_generator_expert';
-                            toolCallbackMessage = toolMessage;
-                            //const expertResponse = await callPathway('sys_generator_expert', { ...args, chatHistory: multiModalChatHistory }, pathwayResolver);
-                            //pathwayResolver.tool = JSON.stringify({ search: false, title });
-                            //return JSON.stringify({response: expertResponse, search: false });
-                        }
+                        toolCallbackName = 'sys_generator_expert';
+                        toolCallbackMessage = toolMessage;
                         break;
                     case "reason":
-                        if (toolRequired) {
-                            toolCallbackName = 'sys_generator_reasoning';
-                            toolCallbackMessage = toolMessage;
-                            //const expertResponse = await callPathway('sys_generator_expert', { ...args, chatHistory: multiModalChatHistory }, pathwayResolver);
-                            //pathwayResolver.tool = JSON.stringify({ search: false, title });
-                            //return JSON.stringify({response: expertResponse, search: false });
-                        }
+                        toolCallbackName = 'sys_generator_reasoning';
+                        toolCallbackMessage = toolMessage;
+                        break;
+                    case "search":
+                    case "document":
+                        toolCallbackName = 'sys_generator_results';
+                        toolCallbackId = null;
+                        toolCallbackMessage = toolMessage;
+                        break;
+                    case "clarify":
+                        toolCallbackName = null;
+                        toolCallbackId = null;
+                        toolCallbackMessage = toolMessage;
                         break;
                     default:
-                        if (searchRequired) {
-                            toolCallbackName = 'sys_generator_results';
-                            toolCallbackId = null;
-                            toolCallbackMessage = toolMessage;
-                        }
+                        toolCallbackName = null;
+                        toolCallbackId = null;
+                        toolCallbackMessage = null;
                         break;
                 }
             }
 
-            if (toolCallbackName) {
+            if (toolCallbackMessage) {
                 pathwayResolver.tool = JSON.stringify({ 
                     hideFromModel: true, 
                     toolCallbackName, 
@@ -220,18 +197,18 @@ export default {
                     codeRequestId,
                     toolCallbackId
                 });
-                return JSON.stringify({response: toolCallbackMessage || "One moment please."});
+                return toolCallbackMessage || "One moment please.";
             }
 
             const chatResponse = await fetchChatResponsePromise;
             pathwayResolver.tool = JSON.stringify({ search: false, title })
-            return JSON.stringify({response: chatResponse});
+            return chatResponse;
 
         } catch (e) {
             pathwayResolver.logError(e);
             const chatResponse = await fetchChatResponsePromise;
             pathwayResolver.tool = JSON.stringify({ search: false, title });
-            return JSON.stringify({response: chatResponse});
+            return chatResponse;
         }
     }
 };
