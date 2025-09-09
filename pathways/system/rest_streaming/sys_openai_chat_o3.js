cf4011ad39b1b224321fff67d7098fd4546b7d77
ademgcl <ademgcl@gmail.com>
2025-09-09 16:17:08 +0300

    feat: add new OpenAI chat model configuration for 'o3'

---
+++ pathways/system/rest_streaming/sys_openai_chat_o3.js
@@ -0,0 +1,22 @@
+// sys_openai_chat_o3.js
+
+import { Prompt } from '../../../server/prompt.js';
+
+export default {
+    prompt:
+    [
+        new Prompt({ messages: [
+            "{{messages}}",
+        ]}),
+    ],
+    inputParameters: {
+        messages: [{role: '', content: []}],
+        functions: '',
+        tools: '',
+        tool_choice: 'auto',
+    },
+    model: 'oai-o3',
+    useInputChunking: false,
+    emulateOpenAIChatModel: 'o3',
+    enableDuplicateRequests: false,
+}
